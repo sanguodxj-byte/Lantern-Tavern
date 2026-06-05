9c2e80ce14b26ff028d506232322ce5062df3b98
Nicolas Bize <nicolasbize@gmail.com>
2026-06-05 16:23:44 -0700

    looking around

--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -1,9 +1,10 @@
 class_name Player
 extends CharacterBody3D
 
-const MAX_ANGLE_LOOK_DOWN := deg_to_rad(-70)
 const MAX_ANGLE_LOOK_UP := deg_to_rad(70)
-const MOUSE_SENSITITY := 0.002
+const MAX_ANGLE_LOOK_DOWN := deg_to_rad(-70)
+
+@export var mouse_sensitivity: float
 
 @onready var camera: Camera3D = %Camera3D
 
@@ -12,9 +13,6 @@ func _ready() -> void:
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		rotate_y(-event.relative.x * MOUSE_SENSITITY)
-		camera.rotate_x(-event.relative.y * MOUSE_SENSITITY)
-		camera.rotation.x = clamp(camera.rotation.x, MAX_ANGLE_LOOK_DOWN, MAX_ANGLE_LOOK_UP)
-
-func _physics_process(delta: float) -> void:
-	move_and_slide()
+		rotate_y(-event.relative.x * mouse_sensitivity) # PI 3.14 => 180 degrees 
+		camera.rotate_x(-event.relative.y * mouse_sensitivity)
+		camera.rotation.x = clampf(camera.rotation.x, MAX_ANGLE_LOOK_DOWN, MAX_ANGLE_LOOK_UP)
