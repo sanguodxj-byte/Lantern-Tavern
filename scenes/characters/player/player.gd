ea746712a2cfd5940fa9160e38d61cec76e4b055
Nicolas Bize <nicolasbize@gmail.com>
2026-06-05 01:21:10 -0700

    looking around

--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -1,2 +1,20 @@
 class_name Player
 extends CharacterBody3D
+
+const MAX_ANGLE_LOOK_DOWN := deg_to_rad(-70)
+const MAX_ANGLE_LOOK_UP := deg_to_rad(70)
+const MOUSE_SENSITITY := 0.002
+
+@onready var camera: Camera3D = %Camera3D
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * MOUSE_SENSITITY)
+		camera.rotate_x(-event.relative.y * MOUSE_SENSITITY)
+		camera.rotation.x = clamp(camera.rotation.x, MAX_ANGLE_LOOK_DOWN, MAX_ANGLE_LOOK_UP)
+
+func _physics_process(delta: float) -> void:
+	move_and_slide()
