ba301d58d274594a5918eae1018deace831483da
Nicolas Bize <nicolasbize@gmail.com>
2026-06-07 20:49:14 -0700

    player movement and reticle

--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -4,15 +4,49 @@ extends CharacterBody3D
 const MAX_ANGLE_LOOK_UP := deg_to_rad(70)
 const MAX_ANGLE_LOOK_DOWN := deg_to_rad(-70)
 
+@export var acceleration: float
+@export var jump_force: float
+@export var gravity: float
 @export var mouse_sensitivity: float
+@export var run_speed: float
+@export var walk_speed: float
 
 @onready var camera: Camera3D = %Camera3D
 
+var input_dir := Vector2.ZERO
+
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _process(_delta: float) -> void:
+	input_dir = Input.get_vector("strafe_left", "strafe_right", "backward", "forward")
+	print(is_on_floor())
+
+func _physics_process(delta: float) -> void:
+	check_jump_input()
+	process_gravity()
+	
+	var input_3d_space := Vector3(input_dir.x, 0, -input_dir.y)
+	var target_speed := run_speed if Input.is_action_pressed("run") else walk_speed
+	var desired_velocity := transform.basis * input_3d_space * target_speed
+	if input_3d_space == Vector3.ZERO:
+		velocity.x = move_toward(velocity.x, 0, acceleration * delta)
+		velocity.z = move_toward(velocity.z, 0, acceleration * delta)
+	else:
+		velocity.x = move_toward(velocity.x, desired_velocity.x, acceleration * delta)
+		velocity.z = move_toward(velocity.z, desired_velocity.z, acceleration * delta)
+	move_and_slide()
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		rotate_y(-event.relative.x * mouse_sensitivity) # PI 3.14 => 180 degrees 
 		camera.rotate_x(-event.relative.y * mouse_sensitivity)
 		camera.rotation.x = clampf(camera.rotation.x, MAX_ANGLE_LOOK_DOWN, MAX_ANGLE_LOOK_UP)
+
+func check_jump_input() -> void:
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y = jump_force
+
+func process_gravity() -> void:
+	if not is_on_floor():
+		velocity.y -= gravity
