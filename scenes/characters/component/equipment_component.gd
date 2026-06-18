ed3d4b19de34fa12e0d11c591c2ee03864010dff
Nicolas Bize <nicolasbize@gmail.com>
2026-06-17 23:03:40 -0700

    fixed material

--- scenes/characters/component/equipment_component.gd
+++ scenes/characters/component/equipment_component.gd
@@ -3,6 +3,7 @@ extends Node3D
 
 const EQUIPED_ITEM_PREFAB := preload("res://scenes/equipment/equiped_item.tscn")
 
+@export var is_always_in_front: bool
 @export var weapon_data: WeaponData
 @export var weapon_placeholder: Node3D
 
@@ -14,4 +15,5 @@ func equip_weapon(data: WeaponData) -> void:
 	weapon_data = data.duplicate()
 	var weapon := EQUIPED_ITEM_PREFAB.instantiate() as EquipedItem
 	weapon.weapon_data = weapon_data
+	weapon.is_always_in_front = is_always_in_front
 	weapon_placeholder.add_child(weapon)
--- scenes/equipment/equiped_item.gd
+++ scenes/equipment/equiped_item.gd
@@ -1,9 +1,15 @@
 class_name EquipedItem
 extends Node3D
 
+const ZCLIP_MATERIAL := preload("res://materials/zclip_material.tres")
+
+@export var is_always_in_front: bool
 @export var weapon_data: WeaponData
 
 func _ready() -> void:
 	var equiped_object := weapon_data.glb_mesh.instantiate()
 	if equiped_object != null:
 		add_child(equiped_object)
+		var mesh_node := equiped_object.get_child(0) as MeshInstance3D
+		if mesh_node != null and is_always_in_front:
+			mesh_node.material_override = ZCLIP_MATERIAL.duplicate()
