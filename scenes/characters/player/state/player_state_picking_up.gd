5343ecf403c4199269e9f637fe123687a173cd2f
Nicolas Bize <nicolasbize@gmail.com>
2026-06-19 14:25:40 -0700

    better pickup

--- scenes/characters/player/state/player_state_picking_up.gd
+++ scenes/characters/player/state/player_state_picking_up.gd
@@ -2,8 +2,12 @@ class_name PlayerStatePickingUp
 extends PlayerState
 
 func _enter_tree() -> void:
+	player.animation_player.play("pickup")
+	player.animation_player.animation_finished.connect(on_animation_finished)
 	var pickable_object := player.current_pickable_focused_item
 	if pickable_object.weapon_data != null:
 		player.equipment.equip_weapon(pickable_object.weapon_data, pickable_object.global_transform)
 		pickable_object.queue_free()
+
+func on_animation_finished(_anim_name: String) -> void:
 	transition_state(Player.State.MOVING)
--- scenes/characters/player/state/player_state_throwing.gd
+++ scenes/characters/player/state/player_state_throwing.gd
@@ -2,5 +2,9 @@ class_name PlayerStateThrowing
 extends PlayerState
 
 func _enter_tree() -> void:
+	player.animation_player.play("throw_weapon")
+	player.animation_player.animation_finished.connect(on_animation_finished)
+
+func on_animation_finished(_anim_name: String) -> void:
 	player.equipment.throw_weapon()
 	transition_state(Player.State.MOVING)
