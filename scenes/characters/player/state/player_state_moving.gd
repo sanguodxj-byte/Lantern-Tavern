a30ea1a276a62e889d4c3a9726e1d87acaf03138
Nicolas Bize <nicolasbize@gmail.com>
2026-06-28 22:45:59 -0700

    added footsteps

--- scenes/characters/player/state/player_state_moving.gd
+++ scenes/characters/player/state/player_state_moving.gd
@@ -1,6 +1,11 @@
 class_name PlayerStateMoving
 extends PlayerState
 
+const DURATION_BETWEEN_FOOTSTEPS_WALK := 500
+const DURATION_BETWEEN_FOOTSTEPS_RUN := 300
+
+var time_since_last_footstep := Time.get_ticks_msec()
+
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("use") and player.can_pickup_object():
 		transition_state(Player.State.PICKING_UP)
@@ -18,5 +23,11 @@ func _physics_process(delta: float) -> void:
 	var horizontal_velocity := Vector3(player.velocity.x, 0, player.velocity.z)
 	if horizontal_velocity.length_squared() > 0.1 and player.is_on_floor():
 		player.animation_player.play("run")
+		var duration := DURATION_BETWEEN_FOOTSTEPS_WALK
+		if Input.is_action_pressed("run"):
+			duration = DURATION_BETWEEN_FOOTSTEPS_RUN
+		if Time.get_ticks_msec() - time_since_last_footstep > duration:
+			AudioManager.play("footstep", player.footstep_audio_stream_player)
+			time_since_last_footstep = Time.get_ticks_msec()
 	else:
 		player.animation_player.play("idle")
