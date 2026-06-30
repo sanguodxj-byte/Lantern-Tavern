0cf5d0abb5101137cecee813197d3877f4e18b06
Nicolas Bize <nicolasbize@gmail.com>
2026-06-30 02:53:47 -0700

    keys in editor

--- scenes/door/door.gd
+++ scenes/door/door.gd
@@ -1,3 +1,4 @@
+@tool
 class_name Door
 extends StaticBody3D
 
--- scenes/rooms/base_room.gd
+++ scenes/rooms/base_room.gd
@@ -1,19 +1,28 @@
+@tool
 class_name BaseRoom
 extends Node3D
 
 const DROPPED_KEY_PREFAB := preload("res://scenes/collectibles/dropped_key/dropped_key.tscn")
 
-@export var key_color: Door.KeyColor
+@export var editor_key_indicator_mesh: MeshInstance3D
+@export var key_color: Door.KeyColor:
+	set(new_color):
+		key_color = new_color
+		editor_update_key_indicator()
 
 @onready var ceilings: GridMap = %Ceilings
+@onready var editor_key_indicator: Node3D = %EditorKeyIndicator
 @onready var enemies: Node3D = %Enemies
 @onready var floors: GridMap = %Floors
 
 var cell_ids_with_no_ceiling := []
 
 func _ready() -> void:
-	fill_ceilings()
-	prep_enemies()
+	if Engine.is_editor_hint():
+		editor_update_key_indicator()
+	else:
+		fill_ceilings()
+		prep_enemies()
 
 func fill_ceilings() -> void:
 	for cell_name : String in ["Ground", "Hole-Corner", "Hole-Side", "Hole-UTurn"]:
@@ -47,3 +56,16 @@ func drop_key(key_transform: Transform3D) -> void:
 	var rand_angle := randf_range(0, PI)
 	var launch_velocity := Vector3(cos(rand_angle) * 2.0, 5.0, sin(rand_angle) * 2.0)
 	key.apply_central_impulse(launch_velocity)
+
+func editor_update_key_indicator() -> void:
+	if Engine.is_editor_hint():
+		editor_key_indicator_mesh.visible = key_color != Door.KeyColor.None
+		if key_color != Door.KeyColor.None:
+			var material := editor_key_indicator_mesh.get_active_material(0).duplicate() as StandardMaterial3D
+			material.albedo_color = Door.COLOR_MAP[key_color]
+			editor_key_indicator_mesh.set_surface_override_material(0, material)
+	else:
+		editor_key_indicator_mesh.visible = false
+	
+	
+	
